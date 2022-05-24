297fcd82901b7aed7ee8aac614964113251a78c7
T.T <t.517a3@gmail.com>
2022-05-24 23:52:14 +0900

    Update Map components

--- src/components/pages/Map.tsx
+++ src/components/pages/Map.tsx
@@ -6,7 +6,6 @@ import {
   Marker,
 } from "@react-google-maps/api";
 import Container from "@mui/material/Container";
-import { Wrapper, Status } from "@googlemaps/react-wrapper";
 
 const containerStyle = {
   height: "60vh",
@@ -18,72 +17,52 @@ const center = {
   lng: 139.77614366422262,
 };
 
-const positionAkiba = {
-  lat: 35.69731,
-  lng: 139.7747,
+const positionTokyo = {
+  lat: 35.62551386235291,
+  lng: 139.77614366422262
 };
 
-const positionIwamotocho = {
-  lat: 35.69397,
-  lng: 139.7762,
+const positionYokohama = {
+  lat: 35.44670550526705,
+  lng: 139.65406751562722
 };
 
 const divStyle = {
   background: "white",
-  fontSize: 7.5,
+  fontSize: 7.5
 };
 
 function Map() {
   const [size, setSize] = useState<undefined | google.maps.Size>(undefined);
   const infoWindowOptions = {
-    pixelOffset: size,
+    pixelOffset: size
   };
   const createOffsetSize = () => {
     return setSize(new window.google.maps.Size(0, -45));
   };
   return (
     <Container sx={{ py: 4 }} maxWidth="md">
-    <LoadScript googleMapsApiKey="AIzaSyDIiOCQLbf1pBeL4JgKiu0gQkdIE6OsfAg" onLoad={() => createOffsetSize()}>
-      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={17}>
-        <Marker position={positionAkiba} />
-        <Marker position={positionIwamotocho} />
-        {/* <InfoWindow position={positionAkiba} options={infoWindowOptions}>
-          <div style={divStyle}>
-            <h1>秋葉原オフィス</h1>
-          </div>
-        </InfoWindow> */}
-        {/* <InfoWindow position={positionIwamotocho} options={infoWindowOptions}>
-          <div style={divStyle}>
-            <h1>岩本町オフィス</h1>
-          </div>
-        </InfoWindow> */}
-      </GoogleMap>
-    </LoadScript>
+      <LoadScript
+        googleMapsApiKey="AIzaSyDIiOCQLbf1pBeL4JgKiu0gQkdIE6OsfAg"
+        onLoad={() => createOffsetSize()}
+      >
+        <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={17}>
+          <Marker position={positionTokyo} />
+          <Marker position={positionYokohama} />
+          <InfoWindow position={positionTokyo} options={infoWindowOptions}>
+            <div style={divStyle}>
+              <h1>ガンダムベース東京</h1>
+            </div>
+          </InfoWindow>
+          <InfoWindow position={positionYokohama} options={infoWindowOptions}>
+            <div style={divStyle}>
+              <h1>GUNDAM FACTORY YOKOHAMA</h1>
+            </div>
+          </InfoWindow>
+        </GoogleMap>
+      </LoadScript>
     </Container>
   );
 };
 
 export default Map;
-
-/**
- * APIキー
- */
-// const API_KEY = "********************"; // TODO: 自分のキーをここに入力
-
-/**
- * サンプルとして地図を表示するコンポーネント
- */
-// function Map(status: Status) {
-//   const [mapProps, setMapProps] = useState<MapProps>(initialMapProps);
-//   return (
-//     <div style={{ width: "100vw", height: "100vh" }}>
-//       <GoogleMapReact
-//         bootstrapURLKeys={{ key: API_KEY }}
-//         center={mapProps.center}
-//         zoom={mapProps.zoom}
-//       />
-//     </div>
-//   );
-// }
-
-// export default Map;

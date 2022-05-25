dd6e1efd4060205f1ae2ce25605f612dbba4d8b4
T.T <t.517a3@gmail.com>
2022-05-26 00:06:48 +0900

    Update Keyword components

--- src/components/pages/Keyword.tsx
+++ src/components/pages/Keyword.tsx
@@ -1,4 +1,5 @@
-import * as React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
@@ -7,15 +8,28 @@ import CardMedia from "@mui/material/CardMedia";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
+import { integerPropType } from "@mui/utils";
 
 const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 function Keyword() {
+  const [shops, setShops] = useState([]);
+  type shops = {
+    id: number;
+    name: string;
+  };
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/api/v1/shops')
+         .then(res => {setShops(res.data)})
+         .catch(error => console.log(error))
+  },[]);
+
   return (
     <Container sx={{ py: 8 }} maxWidth="md">
       <Grid container spacing={4}>
-        {cards.map((card) => (
-          <Grid item key={card} xs={12} sm={6} md={4}>
+        {shops.map((shop: shops) => (
+          <Grid item key={shop.id} xs={12} sm={6} md={4}>
             <Card
               sx={{
                 height: "100%",
@@ -34,7 +48,7 @@ function Keyword() {
               />
               <CardContent sx={{ flexGrow: 1 }}>
                 <Typography gutterBottom variant="h5" component="h2">
-                  Heading
+                  {shop.name}
                 </Typography>
                 <Typography>
                   This is a media card. You can use this section to describe the

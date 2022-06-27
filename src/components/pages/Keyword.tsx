fec48254106b4d549a7e59f67178c9bdb6f472f3
T.T <t.517a3@gmail.com>
2022-06-27 20:48:09 +0900

    Add Search function

--- src/components/pages/Keyword.tsx
+++ src/components/pages/Keyword.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useRef } from "react";
 import "./PageStyles.scss";
 import axios from "axios";
 import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
@@ -14,6 +15,8 @@ import { integerPropType } from "@mui/utils";
 const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 function Keyword() {
+
+  // <Railsからデータ取得>
   const [isAvailable, setAvailable] = useState(false);
   const [shops, setShops] = useState<Shop[]>([]);
   type Shop = {
@@ -41,47 +44,117 @@ function Keyword() {
                console.log("Rails Api からデータを取得");
               })
          .catch(error => console.log(error))
-    },[isAvailable]);
+  },[isAvailable]);
+  // </Railsからデータ取得>
+
+  // <検索機能>
+  // 入力キーワード
+  const [keyword, setKeyword] = useState("");
+  // itemsのListを表示・非表示を切替。onClick で true を渡して表示させる
+  const [showLists, setShowLists] = useState(false);
+  // List 形式で表示するデータ。初期値では検索キーワードを入力していないので上で定義した
+  // shops を全件渡している
+  const [filteredShops, setFilteredShops] = useState<Shop[]>(shops);
+
+  const Search = () => {
+    if (keyword === "") {
+      setFilteredShops(shops);
+      return;
+    }
+
+    const searchKeywords = keyword
+      .trim()
+      .toLowerCase()
+      .match(/[^\s]+/g);
+
+    //入力されたキーワードが空白のみの場合
+    if (searchKeywords === null) {
+      setFilteredShops(shops);
+      return;
+    }
+
+    const result = shops.filter((shop) =>
+      searchKeywords.every((kw) => shop.name.toLowerCase().indexOf(kw) !== -1)
+    );
+
+    const noResult: Shop[] = [
+      {
+        id: 0,
+        name: "No Item Found",
+        lat: 0,
+        lng: 0,
+        address: "",
+        opening_hours: 0,
+        photo_reference: "noimage",
+        rating :0
+      }
+    ];
+
+    setFilteredShops(result.length ? result : noResult);
+  };
+  // </検索機能>
+
 
   return (
-    <Container sx={{ py: 8 }} maxWidth="md">
-      <Grid container spacing={4}>
-        {shops.map((shop) => (
-          <Grid item key={shop.id} xs={12} sm={6} md={4}>
-            <Card
-              sx={{
-                height: "100%",
-                display: "flex",
-                flexDirection: "column"
-              }}
-            >
-              <CardMedia
-                component="img"
-                sx={{
-                  pt: "10%"
-                }}
-                image={shop.photo_reference}
-                alt="image"
-              />
-              <CardContent sx={{ flexGrow: 1 }}>
-                <Typography gutterBottom variant="h5" component="h2">
-                  <p className="shopName">{shop.name}</p>
-                  <p className="shopAddress">{shop.address}</p>
-                  <p className="shopRating">Googleで☆{shop.rating}</p>
-                  <p className="shopOpneHours">営業時間{shop.opening_hours}</p>
-                  latitude: {shop.lat}
-                  longitude: {shop.lng}
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button size="small">いいね！（実装中）</Button>
-                <Button size="small">口コミ（実装中）</Button>
-              </CardActions>
-            </Card>
+    <>
+      <Container sx={{ py: 8 }} maxWidth="md">
+        <Grid container spacing={0} direction="column" alignItems="center">
+          <Grid item xs={3}>
+            <TextField
+              id="field"
+              color="secondary"
+              variant="outlined"
+              label="enter keywords"
+              onChange={(e) => setKeyword(e.target.value)}
+              onClick={() => setShowLists(true)}
+            />
+            <Button variant="outlined" onClick={Search}>
+              検索
+            </Button>
           </Grid>
-        ))}
-      </Grid>
-    </Container>
+        </Grid>
+      {/* </Container>
+
+      <Container sx={{ py: 8 }} maxWidth="md"> */}
+        <Grid container spacing={4}>
+        {showLists &&
+            filteredShops.map((shop, i) => (
+            <Grid item key={i} xs={12} sm={6} md={4}>
+              <Card
+                sx={{
+                  height: "100%",
+                  display: "flex",
+                  flexDirection: "column"
+                }}
+              >
+                <CardMedia
+                  component="img"
+                  sx={{
+                    pt: "10%"
+                  }}
+                  image={shop.photo_reference}
+                  alt="image"
+                />
+                <CardContent sx={{ flexGrow: 1 }}>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    <p className="shopName">{shop.name}</p>
+                    <p className="shopAddress">{shop.address}</p>
+                    <p className="shopRating">Googleで☆{shop.rating}</p>
+                    <p className="shopOpneHours">営業時間{shop.opening_hours}</p>
+                    latitude: {shop.lat}
+                    longitude: {shop.lng}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small">いいね！（実装中）</Button>
+                  <Button size="small">口コミ（実装中）</Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    </>
   );
 }
 

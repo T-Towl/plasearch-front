acb402c691673c9500ab72d8780dad40fbf1946e
T.T <t.517a3@gmail.com>
2022-07-28 00:56:35 +0900

    Add Registration component

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -3,9 +3,11 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Map from "./pages/Map";
 import Shops from "./pages/Shops";
+import ShopDetail from "./pages/ShopDetail";
 import About from "./pages/About";
 import Error from "./pages/Error";
-import ShopDetail from "./pages/ShopDetail";
+import Dashboard from "./pages/Dashboard";
+
 
 function Main() {
   return (
@@ -14,9 +16,10 @@ function Main() {
         <Route index element={<Home />} />
         <Route path="/map" element={<Map />} />
         <Route path="/shops" element={<Shops />} />
+        <Route path="/shopdetail/:id" element={<ShopDetail />} />
         <Route path="/about" element={<About />} />
         <Route path="*" element={<Error />} />
-        <Route path="/shopdetail/:id" element={<ShopDetail />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </main>
   );
---
+++ src/components/auth/Registration.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Registration() {
+    return (
+        <div>
+           <p>新規登録</p>
+        </div>
+    )
+}
